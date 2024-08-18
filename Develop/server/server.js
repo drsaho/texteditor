b24afc6f19b65ee41d34cf8de8b018dc58cbfd37
Amadou Saho <drsaho2@yahoo.com>
2024-08-18 16:06:08 -0500

    edited server

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'client/dist' directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// Middleware for parsing application/x-www-form-urlencoded
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
-
-// Middleware for parsing application/json
 app.use(express.json());
 
-// Import and use routes
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
